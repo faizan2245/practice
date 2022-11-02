3e13055afec67cac930169588a530acf0665881b
faizan2245 <117216745+faizan2245@users.noreply.github.com>
2022-11-02 14:55:39 +0500

    Add files via upload

---
+++ js practice/app.js
@@ -0,0 +1,21 @@
+//Chapter 1 to 8 Practice
+alert("Welcome");
+var a=15;
+var b=15;
+var c=a++;
+var d=a+b;
+var e=b+c;      
+var popularNumber = 3 * 12;             
+console.log(d); //post increment
+console.log(e); //without increment
+console.log(popularNumber);  //multiplication answer
+
+var resultOfComputation = ((2 * 4) * 4) / 2;     //different type of calculation
+console.log(resultOfComputation);
+
+var nam1="Acne";      //legal variable name
+var nam2="Mark";
+var nam3=nam1 + nam2;
+console.log(nam3);         //addition of string;
+
+// var alert 1 =5; // legal variable name
